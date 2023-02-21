6b39d6591ed14c77bd7ac15e30b26f1f2c5fa83d
Sudipa Acharya <sudipaacharya351@gmail.com>
2023-02-22 00:18:47 +0530

    code added

--- type/src/components/Footer/Footer.jsx
+++ type/src/components/Footer/Footer.jsx
@@ -4,14 +4,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer-container">
-            <a
-                target="_blank"
-                className="footer-link"
-                href="https://theleanprogrammer.com/aam"
-                rel="noreferrer"
-            >
-                Watch the full react course now!
-            </a>
+           
         </div>
     );
 };
